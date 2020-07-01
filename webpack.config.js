877e7d554da264fad772cc7d47a717db1ec9c0f8
Yakov Rakhamimov <jacobra19@gmail.com>
2020-07-01 17:50:36 +0300

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,47 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
-        },
-        {
-          test: /\.html$/,
-          use: [
+        rules: [
             {
-              loader: "html-loader"
-            }
-          ]
-        }
-      ]
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ]
     },
-    plugins: [
-      new HtmlWebPackPlugin({
-        template: "./src/index.html",
-        filename: "./index.html"
-      })
-    ],
+    plugins:
+        [
+            new HtmlWebPackPlugin({
+                template: "./src/index.html",
+                filename: "./index.html",
+                favicon: "./src/assets/redux.png",
+            })],
     devServer: {
         port: 2020
     }
-  };
+};
