3890eb0c864b3131bacd47e788a5ff820d6c2954
Yakov Rakhamimov <jacobra19@gmail.com>
2020-07-13 19:25:35 +0300

    add todo - ui

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -1,24 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
+import { Button, Input } from '@material-ui/core'
+
+
 import { addTodo } from '../actions'
 
 const AddTodo = ({ dispatch }) => {
-    let input
+    const [todo, setTodo] = useState("");
+
+    const handleChange = (e) => {
+        setTodo(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (!todo.trim()) return;
+
+        setTodo('')
+        dispatch(addTodo(todo))
+    }
 
     return (
-        <div>
-            <form
-                onSubmit={e => {
-                    e.preventDefault()
-                    if (!input.value.trim()) {
-                        return
-                    }
-                    dispatch(addTodo(input.value))
-                    input.value = ''
-                }}
-            >
-                <input ref={node => (input = node)} />
-                <button type="submit">Add Todo</button>
+        <div style={{width:'100%',marginBottom:30}}>
+            <form onSubmit={handleSubmit} autoComplete={"off"} style={{display:'flex',alignItems:'center',justifyContent:'space-between',width:'100%'}}>
+                <Input  placeholder="Enter Todo"
+                        variant="outlined" 
+                        id="todo-input" 
+                        label="Todo" 
+                        onChange={handleChange} 
+                        value={todo} 
+                        style={{height:36,marginRight:10,width:'-webkit-fill-available'}}
+                />
+                <Button type="submit" variant={'contained'} color={'primary'} disabled={!todo.trim()}>Add</Button>
             </form>
         </div>
     )
