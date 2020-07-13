30a67d2866947e3dd1c8864f498b4c9359035a59
Yakov Rakhamimov <jacobra19@gmail.com>
2020-07-13 19:22:45 +0300

    MuiThemeProvider, redux devtools

--- src/index.js
+++ src/index.js
@@ -1,19 +1,45 @@
+// react
 import React from 'react';
 import ReactDOM from "react-dom";
-import App from './app'
 
+// redux
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import rootReducer from './reducers'
 
+// mui
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+
+import App from './app'
+
 import './style.css';
 
 
-const store = createStore(rootReducer)
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+
+const theme = createMuiTheme({
+    // palette: {
+    //     primary: { main: '#228e22' },
+    //     secondary: { main: '#4d4d4d' },
+    // },
+    typography: {
+        useNextVariants: true,
+        fontFamily: '"Raleway", sans-serif;',
+    },
+    span: {
+        useNextVariants: true,
+        fontFamily: '"Raleway", sans-serif;',
+    },
+})
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <MuiThemeProvider theme={theme}>
+            <App />
+        </MuiThemeProvider>
     </Provider>,
     document.getElementById('root')
 );
