18e375db88e928052bb92f76ff95b8e8f2b983f0
Yakov Rakhamimov <jacobra19@gmail.com>
2020-07-13 19:23:42 +0300

    todo styles and actions

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,39 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Todo = ({ onClick, completed, text }) => (
-    <li
-        onClick={onClick}
-        style={{
-            textDecoration: completed ? 'line-through' : 'none'
+import { Checkbox, Typography, ListItem, Fade, IconButton, Tooltip } from '@material-ui/core'
+import DeleteIcon from '@material-ui/icons/Delete';
+
+const Todo = ({ onClick, onDelete, completed, text }) => (
+    <Fade in={true}>
+        <ListItem style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between'
         }}
-    >
-        {text}
-    </li>
+        >
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                <Checkbox checked={completed}
+                    color={completed ? "default" : "primary"}
+                    inputProps={{ 'aria-label': 'secondary checkbox' }}
+                    onClick={onClick}
+                />
+                <Typography style={{ textDecoration: completed ? 'line-through' : 'none' }}>
+                    {text}
+                </Typography>
+
+            </div>
+            <Tooltip title={`Delete Todo`}>
+
+
+                <IconButton size={'small'}
+                    onClick={onDelete} >
+                    <DeleteIcon style={{ fontSize: 20 }} />
+                </IconButton>
+            </Tooltip>
+
+        </ListItem >
+    </Fade>
 )
 
 Todo.propTypes = {
